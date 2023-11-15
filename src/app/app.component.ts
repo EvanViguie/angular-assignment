63d6d7ea7ee19e731326e6192242c479cb2e8df9
Evan Viguié <evan.viguie@proton.me>
2023-11-15 16:17:08 +0100

    ✨ feat: Add FooterComponent and CguContent imports
    - Import FooterComponent and CguContent in app.component.ts file
    - Update the imports section to include the new components
    - Update the AppComponent class to include the new components in the imports array
    - Update the templateUrl and styleUrls to match the changes
    
    Gitmoji: ✨ (sparkles)
    Commit Message: feat: Add FooterComponent and CguContent imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterOutlet} from '@angular/router';
 import {ToolbarComponent} from "./toolbar/toolbar.component";
+import {FooterComponent} from "./footer/footer.component";
+import {CguContent} from "./cgu/cgu.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, ToolbarComponent],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+    selector: 'app-root',
+    standalone: true,
+    imports: [
+        CommonModule,
+        RouterOutlet,
+        ToolbarComponent,
+        FooterComponent,
+        CguContent
+    ],
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'Localisateur d\'École';
+    title = 'Localisateur d\'École';
 }
