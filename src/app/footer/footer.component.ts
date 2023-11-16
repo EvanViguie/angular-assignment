37776d42c3752937fe7ea2a6b83cca069d0198f8
Evan Viguié <evan.viguie@proton.me>
2023-11-16 10:08:27 +0100

    ✨ :lipstick: Update footer component template and styles
    - Update the template of the footer component to include a footer section with
      copyright information and a footer menu
    - Add a footer menu with links to the terms and conditions page and the home
      page
    - Update the styles of the footer component
    
    Repository: /var/www/angular-assignment

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,11 +6,34 @@ import {MatButtonModule} from '@angular/material/button';
 
 
 @Component({
-    selector: 'app-footer',
-    standalone: true,
-    imports: [CommonModule, RouterLink, RouterOutlet, CguContent, MatButtonModule],
-    templateUrl: './footer.component.html',
-    styleUrl: './footer.component.sass'
+  selector: 'app-footer',
+  standalone: true,
+  imports: [CommonModule, RouterLink, RouterOutlet, CguContent, MatButtonModule],
+  template:
+    `
+      <footer class="footer-section">
+        <div class="col-xl-6 col-lg-6 text-center text-lg-left">
+          <div class="copyright-area">
+            <div class="copyright-text">
+              <p>&copy; 2023, Evan Viguié</p>
+            </div>
+          </div>
+          <div class="col-xl-6 col-lg-6 d-none d-lg-block text-right">
+            <div class="footer-menu">
+              <ul>
+                <li>
+                  <cgu-content></cgu-content>
+                </li>
+                <li>
+                  <button mat-button color="accent" [routerLink]="['/']">Accueil</button>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
+    `,
+  styleUrl: './footer.component.sass'
 })
 export class FooterComponent {
 
