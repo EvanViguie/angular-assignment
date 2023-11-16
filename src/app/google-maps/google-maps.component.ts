1f44793adf85c6cb598c5f50d72fadd873f05c49
Evan Viguié <evan.viguie@proton.me>
2023-11-16 18:03:44 +0100

    ✨:sparkles: feat(google-maps): Add options to Google Maps component
    The commit adds options to the Google Maps component in order to customize the map display. The options include setting the center coordinates, zoom level, and map ID.

--- src/app/google-maps/google-maps.component.ts
+++ src/app/google-maps/google-maps.component.ts
@@ -1,27 +1,30 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable, of} from 'rxjs';
+import {catchError, map} from 'rxjs/operators';
 
 @Component({
     selector: 'google-maps',
     template:
         `
-            <google-map *ngIf="apiLoaded | async"></google-map>
+                <google-map width="100%" height="100%" [options]="options" *ngIf="apiLoaded | async"></google-map>
         `,
 })
 export class GoogleMapsComponent {
+    options: google.maps.MapOptions = {
+        center: {lat: 46.15522996311655, lng: -1.16119538510170113},
+        zoom: 14,
+        mapId: '944fbdb5aa4c11c8',
+    };
     apiLoaded: Observable<boolean>;
 
     constructor(httpClient: HttpClient) {
-        // If you're using the `<map-heatmap-layer>` directive, you also have to include the `visualization` library
-        // when loading the Google Maps API. To do so, you can add `&libraries=visualization` to the script URL:
-        // https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=visualization
 
         this.apiLoaded = httpClient.jsonp('https://maps.googleapis.com/maps/api/js?key=AIzaSyAnCrCZPnx88u26BlTdoqpnzT0tnhB--jk', 'callback')
             .pipe(
                 map(() => true),
                 catchError(() => of(false)),
             );
+
     }
 }
