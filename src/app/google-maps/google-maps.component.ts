ed22e4bfd6cd75eedbad4a0289656cd9376609d7
Evan Viguié <evan.viguie@proton.me>
2023-11-17 16:40:24 +0100

    ✨ feat(google-maps): Add markers and info window to Google Maps component
    - Import MapInfoWindow and MapMarker from @angular/google-maps
    - Define ViewChild for MapInfoWindow
    - Add map-marker element to display markers on the map
    - Set position, label, and title attributes for each marker
    - Add map-info-window element to display info window
    - Update options object to include mapId and comment out disableDefaultUI
    - Define code_commune and api_url variables for API request
    - Initialize markers array
    - Create loadSchools() function to fetch school details from API
    - Push each school location to markers array
    - Set label and title attributes for each marker
    - Call loadSchools() function in constructor
    - Update apiLoaded to use HttpClient
    - Catch errors and set apiLoaded to false if API request fails

--- src/app/google-maps/google-maps.component.ts
+++ src/app/google-maps/google-maps.component.ts
@@ -1,31 +1,68 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable, of} from 'rxjs';
 import {catchError, map} from 'rxjs/operators';
+import { MapInfoWindow, MapMarker} from "@angular/google-maps";
+import {Schooldetails} from "../schooldetails";
 
 @Component({
   selector: 'google-maps',
   template:
     `
-      <google-map width="100%" height="90%" [options]="options" *ngIf="apiLoaded | async"></google-map>
+        <google-map width="100%" height="90%" [options]="options" *ngIf="apiLoaded | async">
+            <map-marker *ngFor="let marker of markers"
+                        [position]="marker.position"
+                        [label]="marker.label"
+                        [title]="marker.title">
+            </map-marker>
+            <map-info-window></map-info-window>
+        </google-map>
     `,
 })
 export class GoogleMapsComponent {
+  @ViewChild(MapInfoWindow, { static: false }) infoWindow!: MapInfoWindow;
+
   options: google.maps.MapOptions = {
     center: {lat: 46.15522996311655, lng: -1.16119538510170113},
     zoom: 14,
     mapId: '944fbdb5aa4c11c8',
-    disableDefaultUI: true,
+    // disableDefaultUI: true,
   };
   apiLoaded: Observable<boolean>;
 
-  constructor(httpClient: HttpClient) {
+  code_commune : string = "17300"
 
-    this.apiLoaded = httpClient.jsonp('https://maps.googleapis.com/maps/api/js?key=AIzaSyAnCrCZPnx88u26BlTdoqpnzT0tnhB--jk', 'callback')
+  api_url : string = "https://data.education.gouv.fr/api/v2/catalog/datasets/fr-en-annuaire-education/records?where=code_commune%3D%22" + this.code_commune + "%22"
+
+  markers: { position: { lat: number, lng: number }, label: { color: string, text: string }, title: string, info: string, }[] = [];
+  loadSchools() {
+    this.http.get<Schooldetails>(this.api_url).subscribe(res => {
+      for (let school of res.records) {
+        console.log(school)
+        this.markers.push({
+          position: {
+            lat: school.record.fields.latitude,
+            lng: school.record.fields.longitude,
+          },
+          label: {
+            color: 'white',
+            text: school.record.fields.nom_etablissement,
+          },
+          title: school.record.fields.nom_etablissement,
+          info: school.record.fields.adresse_1 + ', ' + school.record.fields.type + ', ' + school.record.fields.libelle_nature,
+        });
+      }
+    });
+  }
+
+  constructor(private http: HttpClient) {
+
+      this.loadSchools();
+
+      this.apiLoaded = http.jsonp('https://maps.googleapis.com/maps/api/js?key=AIzaSyAnCrCZPnx88u26BlTdoqpnzT0tnhB--jk', 'callback')
       .pipe(
         map(() => true),
         catchError(() => of(false)),
       );
-
   }
 }
