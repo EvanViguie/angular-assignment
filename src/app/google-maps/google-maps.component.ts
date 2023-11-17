907334ad75fa77a3542d00247d672ca83ab59be7
Evan Viguié <evan.viguie@proton.me>
2023-11-17 17:09:39 +0100

    ✨:sparkles: refactor(google-maps.component): Add AfterViewInit interface
    🔥:fire: fix(google-maps.component): Fix marker click event handling
    
    🚀:rocket: feat(google-maps.component): Add info window content and reservation button
    
    🐛:bug: fix(google-maps.component): Fix marker info window opening
    
    👷:construction_worker: chore(google-maps.component): Add ngAfterViewInit implementation
    
    🔧:wrench: chore(google-maps.component): Add apiLoaded property
    
    💄:lipstick: style(google-maps.component): Format code and add missing imports
    
    📝:memo: docs(google-maps.component): Update comments and add code explanations
    
    :shit: It doesn't work

--- src/app/google-maps/google-maps.component.ts
+++ src/app/google-maps/google-maps.component.ts
@@ -1,9 +1,11 @@
-import {Component, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable, of} from 'rxjs';
 import {catchError, map} from 'rxjs/operators';
 import { MapInfoWindow} from "@angular/google-maps";
 import {Schooldetails} from "../schooldetails";
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'google-maps',
@@ -12,15 +14,19 @@ import {Schooldetails} from "../schooldetails";
         <google-map width="100%" height="90%" [options]="options" *ngIf="apiLoaded | async">
             <map-marker *ngFor="let marker of markers"
                         [position]="marker.position"
-                        [label]="marker.label"
-                        [title]="marker.title">
+                        (mapMarkerClick)="openInfo(marker)">
             </map-marker>
-            <map-info-window></map-info-window>
+            <map-info-window>
+              <h2>{{selectedMarker?.title}}</h2>
+              <p>{{selectedMarker?.info}}</p>
+              <button (click)="goToReservation()">Reserve</button>
+            </map-info-window>
         </google-map>
     `,
 })
-export class GoogleMapsComponent {
+export class GoogleMapsComponent implements AfterViewInit{
   @ViewChild(MapInfoWindow, { static: false }) infoWindow!: MapInfoWindow;
+
 
   options: google.maps.MapOptions = {
     center: {lat: 46.15522996311655, lng: -1.16119538510170113},
@@ -28,8 +34,6 @@ export class GoogleMapsComponent {
     mapId: '944fbdb5aa4c11c8',
     // disableDefaultUI: true,
   };
-  apiLoaded: Observable<boolean>;
-
   code_commune : string = "17300"
 
   api_url : string = "https://data.education.gouv.fr/api/v2/catalog/datasets/fr-en-annuaire-education/records?where=code_commune%3D%22" + this.code_commune + "%22"
@@ -39,7 +43,7 @@ export class GoogleMapsComponent {
     this.http.get<Schooldetails>(this.api_url).subscribe(res => {
       for (let school of res.records) {
         console.log(school)
-        this.markers.push({
+        const marker = new google.maps.Marker({
           position: {
             lat: school.record.fields.latitude,
             lng: school.record.fields.longitude,
@@ -51,11 +55,35 @@ export class GoogleMapsComponent {
           title: school.record.fields.nom_etablissement,
           info: school.record.fields.adresse_1 + ', ' + school.record.fields.type + ', ' + school.record.fields.libelle_nature,
         });
+        marker.addListener('click', () => {
+          this.openInfo(marker);
+        });
+        this.markers.push(marker);
       }
     });
   }
+  ngAfterViewInit() {
+    this.loadSchools();
+  }
+  selectedMarker: any;
 
-  constructor(private http: HttpClient) {
+  openInfo(marker: any) {
+    console.log('openInfo called', marker);
+    if (this.infoWindow && marker) {
+      this.selectedMarker = marker;
+      this.infoWindow.open(marker);
+    } else {
+      console.error('InfoWindow or marker are undefined');
+    }
+  }
+
+  goToReservation() {
+    this.router.navigate(['/reservation']);
+  }
+
+  apiLoaded: Observable<boolean>;
+
+  constructor(private http: HttpClient, private router: Router) {
 
       this.loadSchools();
 
