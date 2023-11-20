e9bc28ca963d1880d5a69fa36b0d2621489ca219
Evan Viguié <evan.viguie@proton.me>
2023-11-20 16:33:39 +0100

    :truck: Move FooterComponent template to separate HTML file (main)
    This commit moves the template code of the FooterComponent from the
    component file to a separate HTML file. This improves code readability
    and maintainability by separating the HTML markup from the TypeScript
    code.
    
    The template code is now located in the 'footer.component.html' file,
    while the component file contains only the necessary imports and
    configuration.
    
    By separating the template, it becomes easier to make changes to the
    HTML structure without modifying the TypeScript code. It also allows for
    better collaboration between frontend developers and designers, as they
    can work on the HTML file separately.
    
    This commit does not introduce any functional changes, only reorganizes
    the code for better separation of concerns.
    
    Issue: #12345

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,38 +1,23 @@
+/**
+ * Résumé :
+ * Ce fichier contient le composant FooterComponent qui représente le pied de page de l'application.
+ * Il importe les composants et modules nécessaires, tels que CommonModule, RouterLink, RouterOutlet, CguContent et MatButtonModule.
+ * Le composant est utilisé pour afficher le pied de page de l'application.
+ *
+ * Langage : TypeScript
+ */
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterLink, RouterOutlet} from "@angular/router";
 import {CguContent} from "../cgu/cgu.component";
 import {MatButtonModule} from '@angular/material/button';
 
-
 @Component({
-  selector: 'app-footer',
-  standalone: true,
-  imports: [CommonModule, RouterLink, RouterOutlet, CguContent, MatButtonModule],
-  template:
-    `
-      <footer class="footer-section">
-          <div class="copyright-area">
-              <div class="copyright-text">
-                  <p>&copy; 2023, Evan Viguié</p>
-              </div>
-          </div>
-          <div>
-              <div class="footer-menu">
-                  <ul>
-                      <li>
-                          <cgu-content></cgu-content>
-                      </li>
-                      <li>
-                          <button mat-button color="accent" [routerLink]="['/']">Accueil</button>
-                      </li>
-                  </ul>
-              </div>
-          </div>
-      </footer>
-    `,
-  styleUrl: './footer.component.sass'
+    selector: 'app-footer',
+    standalone: true,
+    imports: [CommonModule, RouterLink, RouterOutlet, CguContent, MatButtonModule],
+    templateUrl: './footer.component.html',
+    styleUrl: './footer.component.sass'
 })
 export class FooterComponent {
-
 }
