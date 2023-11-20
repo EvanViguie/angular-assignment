499baae26653144e3143d71a9ca8f1f9082731c2
Evan Viguié <evan.viguie@proton.me>
2023-11-20 16:29:48 +0100

    🚀 Bootstrap Angular application and provide Protractor testing support and route configuration
    - Bootstrap the Angular application
    - Provide Protractor testing support
    - Provide route configuration for the application

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
-import { bootstrapApplication, provideProtractorTestingSupport } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
+/**
+ * Résumé :
+ * Ce fichier contient le code responsable du démarrage de l'application Angular.
+ * Langage : TypeScript
+ */
+import {bootstrapApplication, provideProtractorTestingSupport} from '@angular/platform-browser';
+import {AppComponent} from './app/app.component';
 import {provideRouter} from '@angular/router';
 import routeConfig from './app/routes';
 
 bootstrapApplication(AppComponent,
-    {
-        providers: [
-            provideProtractorTestingSupport(),
-            provideRouter(routeConfig)
-        ]
-    }
+  {
+    providers: [
+      provideProtractorTestingSupport(), // Fournit un support pour les tests avec Protractor
+      provideRouter(routeConfig) // Fournit la configuration des routes pour l'application
+    ]
+  }
 ).catch((err) => console.error(err));
-
-
-
