1aad1b0185206f00ab6dc0627455651648cc62fe
Evan Viguié <evan.viguie@proton.me>
2023-11-21 17:11:12 +0100

    ✨ Add Leaflet map component (master)
    This commit adds an Angular component that displays a map using the Leaflet library. The component utilizes the MarkerService to add markers to the map. The map is initialized in the initMap() method by setting the center and zoom. Then, an OpenStreetMap tile layer is added to the map. The component implements the AfterViewInit interface to ensure that the map is initialized before adding the markers.
    
    - Added Leaflet map component.
    - Initialized the map with center and zoom.
    - Added OpenStreetMap tile layer.
    - Utilized MarkerService to add markers.
    - Implemented AfterViewInit interface.

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,50 +1,64 @@
-import { Component, AfterViewInit } from '@angular/core';
+/**
+ * Ce code est un composant Angular qui affiche une carte utilisant la bibliothèque Leaflet.
+ * Il utilise le service MarkerService pour ajouter des marqueurs sur la carte.
+ * La carte est initialisée dans la méthode initMap() en définissant le centre et le zoom.
+ * Ensuite, une couche de tuiles OpenStreetMap est ajoutée à la carte.
+ * Le composant implémente l'interface AfterViewInit pour s'assurer que la carte est initialisée avant d'ajouter les marqueurs.
+ */
+import {Component, AfterViewInit, ChangeDetectionStrategy} from '@angular/core';
 import * as L from 'leaflet';
-import { MarkerService } from '../marker.service';
+import {MarkerService} from '../marker.service';
+import {DomSanitizer} from "@angular/platform-browser";
 
 const iconRetinaUrl = 'assets/marker-icon-2x.png';
 const iconUrl = 'assets/marker-icon.png';
 const shadowUrl = 'assets/marker-shadow.png';
 const iconDefault = L.icon({
-    iconRetinaUrl,
-    iconUrl,
-    shadowUrl,
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    tooltipAnchor: [16, -28],
-    shadowSize: [41, 41]
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
 });
 L.Marker.prototype.options.icon = iconDefault;
 
 @Component({
-    selector: 'app-map',
-    templateUrl: './map.component.html',
-    styleUrls: ['./map.component.sass'],
-    standalone: true,
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrl: './map.component.sass',
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MapComponent implements AfterViewInit {
-    private map!: L.Map;
+  private map!: L.Map;
 
-    constructor(private markerService: MarkerService) { }
+  constructor(private markerService: MarkerService, private sanitizer: DomSanitizer) {
+  }
 
-    private initMap(): void {
-        this.map = L.map('map', {
-            center: [ 39.8282, -98.5795 ],
-            zoom: 3
-        });
+  private initMap(): void {
+    let x: number = 46.1568;
+    let y: number = -1.1708;
+    let z: number = 14;
 
-        const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 18,
-            minZoom: 3,
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        });
+    this.map = L.map('map', {
+      center: [x, y],
+      zoom: z
+    });
 
-        tiles.addTo(this.map);
-    }
+    const tiles :L.TileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
 
-    ngAfterViewInit(): void {
-        this.initMap();
-        this.markerService.makeCapitalCircleMarkers(this.map);
-    }
+    tiles.addTo(this.map);
+  }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+    this.markerService.makeCapitalMarkers(this.map);
+  }
 }
