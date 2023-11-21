95aa0d7b06bc161b8fa4e5fdd9e60bf24297d212
Evan Viguié <evan.viguie@proton.me>
2023-11-21 17:11:38 +0100

    :sparkles: Add Google Maps module and component to HomeComponent (master)
    This commit adds the Google Maps module and component to the HomeComponent. The HomeComponent is responsible for displaying the home page of the application. By importing the GoogleMaps module and using the GoogleMaps component, a map is displayed on the home page.
    
    The changes were made to the src/app/home/home.component.ts file.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,6 @@
 /**
- * Résumé :
  * Ce fichier contient la classe HomeComponent, qui est responsable de l'affichage de la page d'accueil de l'application.
  * Il importe le module GoogleMaps et utilise le composant GoogleMaps pour afficher une carte sur la page d'accueil.
- *
- * Langage : TypeScript
  */
 import {Component} from '@angular/core';
 import {MapComponent} from "../map/map.component";
