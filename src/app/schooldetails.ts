52e5adaa9cbe2f220be94af3adb7ca96b4c5d22f
Evan Viguié <evan.viguie@proton.me>
2023-11-21 17:09:40 +0100

    ✨feat: add structure for school details (angular-assignment) This commit adds an interface "Schooldetails" to define the structure of a school's details. The interface includes properties for the school's identifier, name, position (longitude and latitude), address, type, and public/private status.
    The purpose of this change is to provide a standardized structure for storing and accessing school details in the application.

--- src/app/schooldetails.ts
+++ src/app/schooldetails.ts
@@ -1,3 +1,15 @@
+/**
+ * Ce fichier contient une interface "Schooldetails" qui définit la structure des détails d'une école.
+ */
 export interface Schooldetails {
-    records: any[];
+    identifiant_de_l_etablissement: string;
+    nom_etablissement: string;
+    position: {
+        lon: number;
+        lat: number;
+    };
+    adresse_1: string;
+    type_etablissement: string;
+    statut_public_prive: string;
 }
+
