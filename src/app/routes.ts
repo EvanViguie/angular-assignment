01c8789c3a2c176519aca960903ac2bcdc99973e
Evan Viguié <evan.viguie@proton.me>
2023-11-22 18:12:40 +0100

    Refactor routes.ts to include MapComponent and change DetailsComponent title to 'Détails Écoles'.

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,18 +1,17 @@
 import {Routes} from "@angular/router";
-import {HomeComponent} from "./home/home.component";
-// import {DetailsComponent} from "./details/details.component";
+import {MapComponent} from "./map/map.component";
+import {DetailsComponent} from "./details/details.component"
 
 const routeConfig: Routes = [
-    {
-        path:'',
-        component: HomeComponent,
-        title: 'School Finder'
-    },
-    // {
-    //     path: 'details/:id',
-    //     component: DetailsComponent,
-    //     title: 'Home details'
-    // }
+  {
+    path: '',
+    component: MapComponent,
+    title: 'School Finder'
+  },
+  {
+    path: 'details/:id',
+    component: DetailsComponent,
+    title: 'Détails Écoles'
+  }
 ];
-
 export default routeConfig;
