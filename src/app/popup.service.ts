d4622252db439ab75fd616259c02d41263d3315e
Evan Viguié <evan.viguie@proton.me>
2023-11-23 18:39:47 +0100

    Refactor and improve string formatting in PopupService
    The PopupService class has been modified to clean up and refine the structure of returned strings. Spaces have been added to improve readability and clarity in returned data formatting, and id selectors have been changed to class selectors for a more flexible style and improved usability.

--- src/app/popup.service.ts
+++ src/app/popup.service.ts
@@ -4,13 +4,14 @@ import {Injectable} from '@angular/core';
 })
 
 export class PopupService {
-  constructor() {
-  }
+  constructor() { }
+
   makeSchoolPopup(data: any): string {
-    return `<div>Nom : ${data.nom_etablissement}</div>` +
-        `<div>Adresse : ${data.adresse_1}</div>` +
-        `<div>Type : ${data.type_etablissement}</div>` +
-        `<div>Statut : ${data.statut_public_prive}</div>` +
-        `<div id="school-id" style="display: none;">${data.identifiant_de_l_etablissement}</div>` +
-        `<button  id="details-button" style="background-color: #3f51b5; color: white; padding: 10px 20px; border: none; border-radius: 4px; cursor: pointer">Contact</button>`;  }
+    return `<div>Nom: ${data.nom_etablissement}</div>
+            <div>Adresse: ${data.adresse_1}</div>
+            <div>Type: ${data.type_etablissement}</div>
+            <div>Statut: ${data.statut_public_prive}</div>
+            <div class="school-id" style="display: none;">${data.identifiant_de_l_etablissement}</div>
+            <button class="details-button" style="background-color: #3f51b5; color: white; padding: 10px 20px; border: none; border-radius: 4px; cursor: pointer">Contact</button>`;
+  }
 }
