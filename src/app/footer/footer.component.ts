8b9dea30d70dd0c5cdf976d4a30678cde114a50a
Evan Viguié <evan.viguie@proton.me>
2023-11-24 20:03:25 +0100

    Update imports and comments in footer component
    Comments and imports in the footer component have been updated for improved clarity. The 'CommonModule' and 'CguContent' were removed and replaced with 'TnCContent', a Terms and Conditions component. Comments have been updated to better reflect these changes.

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,18 +1,19 @@
 /**
  * Ce fichier contient le composant FooterComponent qui représente le pied de page de l'application.
- * Il importe les composants et modules nécessaires, tels que CommonModule, RouterLink, RouterOutlet, CguContent et MatButtonModule.
- * Le composant est utilisé pour afficher le pied de page de l'application.
+ * Nous avons importé les packages nécessaires pour ce composant, notamment 'Component' de '@angular/core' qui est essentiel pour tout composant Angular,
+ * ainsi que 'RouterLink' et 'RouterOutlet' de '@angular/router' qui sont utilisés pour la navigation dans l'application.
+ * 'TnCContent' fait référence à notre composant Conditions Générales d'Utilisation, et 'MatButtonModule' est un module
+ * de Material Angular pour utiliser les boutons Material Design.
  */
 import {Component} from '@angular/core';
-import {CommonModule} from '@angular/common';
 import {RouterLink, RouterOutlet} from "@angular/router";
-import {CguContent} from "../cgu/cgu.component";
+import {TnCContent} from "../cgu/tnc.component";
 import {MatButtonModule} from '@angular/material/button';
 
 @Component({
     selector: 'app-footer',
     standalone: true,
-    imports: [CommonModule, RouterLink, RouterOutlet, CguContent, MatButtonModule],
+    imports: [RouterLink, RouterOutlet, TnCContent, MatButtonModule],
     templateUrl: './footer.component.html',
     styleUrl: './footer.component.sass'
 })
