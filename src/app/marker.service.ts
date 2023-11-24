991b08ebbd76b4ce76dba11b73851ce356b149ac
Evan Viguié <evan.viguie@proton.me>
2023-11-24 20:00:59 +0100

    Refactor Marker Service to increase maintainability
    This commit divides the original 'makeSchoolsMarkers' function into several smaller reusable functions. It also moves the API URL to a class property for better visibility. Additionally, the revised functions return Observables to support more predictable data flow and error handling.

--- src/app/marker.service.ts
+++ src/app/marker.service.ts
@@ -6,18 +6,19 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {PopupService} from './popup.service';
 import {Schooldetails} from "./schooldetails";
-import {take} from "rxjs/operators"
+import { Observable } from 'rxjs';
 import * as L from 'leaflet';
 import {Router} from "@angular/router";
+import {take, map} from "rxjs/operators";
 
-const SCHOOLS_API_URL: string = 'https://data.education.gouv.fr/api/explore/v2.1/catalog/datasets/fr-en-annuaire-education/records?select=*&where=nom_commune%3' +
-  'D%20%20%22La%20Rochelle%22%20&limit=100&refine=type_etablissement%3A%22Ecole%22&refine=type_etablissement%3A%22Coll%C3%A8ge%22&refine=type_' +
-  'etablissement%3A%22Lyc%C3%A9e%22';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MarkerService {
+  private readonly SCHOOLS_API_URL: string = 'https://data.education.gouv.fr/api/explore/v2.1/catalog/datasets/fr-en-annuaire-education/records?select=*&where=nom_commune%3' +
+    'D%20%20%22La%20Rochelle%22%20&limit=100&refine=type_etablissement%3A%22Ecole%22&refine=type_etablissement%3A%22Coll%C3%A8ge%22&refine=type_' +
+    'etablissement%3A%22Lyc%C3%A9e%22';
   constructor(
     // Injection des dépendances
     private http: HttpClient,
@@ -26,60 +27,62 @@ export class MarkerService {
   ) {
   }
 
-  /**
-   * Crée des marqueurs pour les écoles sur une carte.
-   * @param map La carte sur laquelle ajouter les marqueurs.
-   */
-  makeSchoolsMarkers(map: L.Map): void {
-    // Récuperation des données des écoles
-    this.http.get<{ results: Schooldetails[] }>(SCHOOLS_API_URL)
-      .pipe(take(1))
-      .subscribe((
-          res: { results: Schooldetails[] }
-        ): void => {
-          res.results.forEach(schoolDetails => this.createMarkerForSchool
-            (schoolDetails, map)
-          );
-        },
+  fetchSchoolData(): Observable<Schooldetails[]> {
+    return this.http.get<{ results: Schooldetails[] }>(this.SCHOOLS_API_URL)
+      .pipe(
+        map(response => response.results),
+        take(1)
       );
   }
 
+  addMarkers(schoolData: Schooldetails[], map: L.Map): Schooldetails[] {
+    schoolData.forEach(school => this.createMarkerForSchool(school, map));
+    return schoolData;
+  }
+
   private createMarkerForSchool(schoolDetails: Schooldetails, map: L.Map): void {
+    const marker: L.Marker<L.LatLngExpression> = this.constructSchoolMarker(schoolDetails);
+    marker.addTo(map);
+    marker.on('popupopen', this.handlePopupOpen.bind(this));
+    marker.on('popupclose', () => this.handlePopupClose(marker));
+  }
+
+  private constructSchoolMarker(schoolDetails: Schooldetails): L.Marker<L.LatLngExpression> {
     const longitude: number = schoolDetails.position.lon;
     const latitude: number = schoolDetails.position.lat;
     const marker: L.Marker<L.LatLngExpression> = L.marker([latitude, longitude]);
-
     marker.bindPopup(this.popupService.makeSchoolPopup(schoolDetails));
-    marker.addTo(map);
+    return marker;
+  }
 
-    marker.on('popupopen', async (e) => {
-      try {
-        const popUp = e.popup;
-        const content: HTMLElement | undefined = popUp.getElement();
-
-        if (content) {
-          const detailsButton: HTMLElement | null = content.querySelector('.details-button');
-          const schoolIdElement: HTMLElement | null = content.querySelector('.school-id');
-
-          if (detailsButton && schoolIdElement) {
-            detailsButton.addEventListener('click', async (): Promise<boolean | void> => {
-              const schoolId: string = schoolIdElement.textContent ?? '';
-
-              try {
-                return await this.router.navigate(['/form', schoolId]);
-              } catch (navigationError) {
-                console.error(navigationError)
-              }
-            });
-          }
+  private async handlePopupOpen(e: L.LeafletEvent): Promise<void> {
+    try {
+      const popUp = e.popup;
+      const content: HTMLElement | undefined = popUp.getElement();
+      if (content) {
+        const detailsButton: HTMLElement | null = content.querySelector('.details-button');
+        const schoolIdElement: HTMLElement | null = content.querySelector('.school-id');
+        if (detailsButton && schoolIdElement) {
+          this.addDetailsButtonClickListener(detailsButton, schoolIdElement);
         }
-      } catch (error) {
-        console.error(error);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  private addDetailsButtonClickListener(detailsButton: HTMLElement, schoolIdElement: HTMLElement): void {
+    detailsButton.addEventListener('click', async (): Promise<boolean | void> => {
+      const schoolId: string = schoolIdElement.textContent ?? '';
+      try {
+        return await this.router.navigate(['/form', schoolId]);
+      } catch (navigationError) {
+        console.error(navigationError);
       }
     });
-    marker.on('popupclose', () => {
-      marker.clearAllEventListeners();
-    });
+  }
+
+  private handlePopupClose(marker: L.Marker): void {
+    marker.clearAllEventListeners();
   }
 }
-
