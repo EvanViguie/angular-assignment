97892233b74cbb424ffce242eb0c8383c74f2dbe
Evan Viguié <evan.viguie@proton.me>
2023-11-27 12:25:39 +0100

    Refactor popup service and enhance school information display
    Refactored the PopupService code to improve readability and incorporated a method to generate an HTML string based on school data. This method enhances the presentation of school data in pop-ups. Also added detailed comments for better code understanding and maintenance.

--- src/app/popup.service.ts
+++ src/app/popup.service.ts
@@ -1,12 +1,22 @@
+/** Ce service est conçu pour fournir des méthodes qui permettent de créer des popups pour les écoles.
+ * Il est marqué comme injectable et est fourni dans le scope 'root', ce qui signifie qu'une seule instance
+ * de ce service est créée et utilisée dans toute l'application.
+ */
+// Importation du décorateur Injectable d'Angular, qui permet d'injecter ce service dans d'autres classes.
 import {Injectable} from '@angular/core';
+
+// Utilisation du décorateur Injectable pour marquer la classe PopupService comme un service qui peut être injecté.
 @Injectable({
   providedIn: 'root'
 })
 
 export class PopupService {
-  constructor() { }
+  constructor() {
+  }
 
+  // Définition de la méthode makeSchoolPopup, qui génère une chaîne de caractères HTML sur base des données d'une école.
   makeSchoolPopup(data: any): string {
+    // Retour d'une chaîne de caractères contenant du code HTML.
     return `<div>Nom: ${data.nom_etablissement}</div>
             <div>Adresse: ${data.adresse_1}</div>
             <div>Type: ${data.type_etablissement}</div>
