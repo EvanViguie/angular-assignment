443ffb39afedd432545cb2af78a767df4bad21f0
Evan Viguié <evan.viguie@proton.me>
2023-11-27 12:27:19 +0100

    Update comments and formatting in tnc.component.ts
    The TypeScript file tnc.component.ts has been updated to provide clearer descriptions of its main functions, dependencies and components. Additional comments have been included for better understanding of the logic and functionality. Code formatting has been improved for better readability.

--- src/app/cgu/tnc.component.ts
+++ src/app/cgu/tnc.component.ts
@@ -1,23 +1,29 @@
 /*
-Ce fichier TypeScript est principalement destiné à gérer l'ouverture des modales et des dialogues dans l'application Angular.
-Il définit deux services : DialogService et ModalService. DialogService est utilisé pour gérer le composant MatDialog
-d'Angular Material qui est utilisé pour ouvrir les dialogues. De l'autre côté, ModalService utilise DialogService pour ouvrir les modales.
-Lorsqu'il est question d'ouvrir une modale ou un dialogue, ces services nécessitent un composant entrant pour déterminer le contenu à afficher.
-Ensuite, nous avons défini un composant 'TnCContent' qui affiche un bouton, et au clic sur ce bouton, un modal contenant les Termes et Conditions
+Ce fichier TypeScript est principalement destiné à gérer l'ouverture des modales et des dialogues dans
+l'application Angular. Il définit deux services : DialogService et ModalService. DialogService est utilisé pour gérer
+le composant MatDialog d'Angular Material qui est utilisé pour ouvrir les dialogues. De l'autre côté, ModalService
+utilise DialogService pour ouvrir les modales. Lorsqu'il est question d'ouvrir une modale ou un dialogue, ces services
+nécessitent un composant entrant pour déterminer le contenu à afficher. Ensuite, nous avons défini un composant
+'TnCContent' qui affiche un bouton, et au clic sur ce bouton, un modal contenant les Termes et Conditions
 est ouvert. Enfin, 'TnCContentDialog' est le composant qui contient le contenu réel à afficher dans le modal.
 */
 
 // Importation des composants et services nécessaires
+// Importation des composants nécessaires à partir du package '@angular/core'
 import {Component, Injectable, Type} from '@angular/core';
+// Importation du module MatDialog de '@angular/material/dialog'
+// permettant d'ajouter des boîtes de dialogue dans l'application
 import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+// Importation du module MatButtonModule de '@angular/material/button'
+// permettant d'ajouter des boutons dans l'application
 import {MatButtonModule} from '@angular/material/button';
 
 // Service pour gérer les Dialogs
 @Injectable({providedIn: 'root'})
 export class DialogService {
+  // Utilise MatDialog pour créer des dialogues
   constructor(private dialog: MatDialog) {
-  } // Utilise MatDialog pour créer des dialogues
-
+  }
   // Ouvre un dialogue basé sur le composant spécifié
   open<T>(component: Type<T>) {
     return this.dialog.open(component);
@@ -27,8 +33,9 @@ export class DialogService {
 // Service pour gérer des Modals
 @Injectable({providedIn: 'root'})
 export class ModalService {
+  // Utilise DialogService pour créer des modales
   constructor(private dialogService: DialogService) {
-  }// Utilise DialogService pour créer des modales
+  }
 
   // Ouvre un modal basé sur le composant spécifié
   openModal<T>(component: Type<T>) {
